15a9a008e00a6c90f57bdc373afb2b745f15f531
ovinransika <ovinransika9172@gmail.com>
2022-05-04 13:38:41 +0530

    Created adding police functionality

---
+++ app/src/main/java/com/example/rda_app/AddPolice.kt
@@ -0,0 +1,9 @@
+package com.example.rda_app
+
+data class AddPolice(
+    val district: String? = null,
+    val SP: String? = null,
+    val address: String? = null,
+    val phone: String? = null,
+    val email: String? = null,
+    val type: String? = null)
--- app/src/main/java/com/example/rda_app/AddPoliceFragment.kt
+++ app/src/main/java/com/example/rda_app/AddPoliceFragment.kt
@@ -1,10 +1,16 @@
 package com.example.rda_app
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.rda_app.databinding.FragmentAddPoliceBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +27,20 @@ class AddPoliceFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    // assign the _binding variable initially to null and
+    // also when the view is destroyed again it has to be set to null
+    private var _binding: FragmentAddPoliceBinding? = null
+
+    // with the backing property of the kotlin we extract
+    // the non null value of the _binding
+    private val binding get() = _binding!!
+
+    //Firebase
+    private lateinit var fStore: FirebaseFirestore
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var email = ""
+    private var password = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,8 +53,83 @@ class AddPoliceFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_police, container, false)
+        // inflate the layout and bind to the _binding
+        _binding = FragmentAddPoliceBinding.inflate(inflater, container, false)
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //handle click, begin register
+        binding.btnRegister.setOnClickListener {
+
+            //validate data
+            validateData()
+            clearTextBoxes()
+        }
+
+        return binding.root
+    }
+
+    private fun clearTextBoxes() {
+        binding.txtDistrict.text.clear()
+        binding.txtSP.text.clear()
+        binding.txtAddress.text.clear()
+        binding.txtPhone.text.clear()
+        binding.txtEmail.text.clear()
+        binding.txtPassword.text.clear()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun validateData() {
+
+        email = binding.txtEmail.text.toString().trim()
+        password = binding.txtPassword.text.toString().trim()
+
+        //Validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            //invalid email format
+            binding.txtEmail.error = "Invalid email format"
+        } else if (TextUtils.isEmpty(password)) {
+            //Password is not entered
+            binding.txtPassword.error = "Please enter password"
+        } else if (password.length < 6) {
+            //password is short
+            binding.txtPassword.error = "Password must be at least more than 6 characters"
+        } else {
+            firebaseRegister()
+        }
+    }
+
+    private fun firebaseRegister() {
+        //create account
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                //get current user
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                val userId = firebaseUser.uid
+
+                val district = binding.txtDistrict.text.toString()
+                val SP = binding.txtSP.text.toString()
+                val address = binding.txtAddress.text.toString()
+                val phone = binding.txtPhone.text.toString()
+                val type = "police"
+
+                fStore = FirebaseFirestore.getInstance()
+                val register = AddPolice(district, SP, address, phone, email, type)
+                fStore.collection("users").document(userId).set(register)
+
+                Toast.makeText(context ,"Registered police with $email", Toast.LENGTH_SHORT).show()
+
+            }
+            .addOnFailureListener { e ->
+                //Failed to register
+                Toast.makeText(context ,"Registration failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
     companion object {
