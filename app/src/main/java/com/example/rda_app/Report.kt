1cd15562606b1af84b5efc483cffb9e237db5159
TheRedMaRK <pmmrkumaradasa@students.nsbm.ac.lk>
2022-05-04 11:39:54 +0530

    - Users can add a report now
    TODO
    
    - Add photos to the report
    - View my reports

---
+++ app/src/main/java/com/example/rda_app/Report.kt
@@ -0,0 +1,9 @@
+package com.example.rda_app
+
+data class Report(
+    val userId: String? = null,
+    val location: String? = null,
+    val date: String? = null,
+    val time: String? = null,
+    val incidentDetails: String? = null
+)
--- app/src/main/java/com/example/rda_app/ReportAccidentActivity.kt
+++ app/src/main/java/com/example/rda_app/ReportAccidentActivity.kt
@@ -2,10 +2,59 @@ package com.example.rda_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.rda_app.databinding.ActivityReportAccidentBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ReportAccidentActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReportAccidentBinding
+
+    //Database variable declaration
+    private lateinit var fStore: FirebaseFirestore
+
+    //FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report_accident)
+        binding = ActivityReportAccidentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //init firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Firebase fireStore auth
+        fStore = FirebaseFirestore.getInstance()
+
+        binding.btnSubmit.setOnClickListener {
+            submitReport()
+        }
+    }
+
+    private fun submitReport() {
+        val firebaseUser = firebaseAuth.currentUser
+        firebaseUser!!.email
+        val userId = firebaseUser.uid
+
+        val location = binding.txtLocation.text.toString()
+        val date = binding.txtDate.text.toString()
+        val time = binding.txtTime.text.toString()
+        val incidentDetails = binding.txtIncidentDetails.text.toString()
+
+        val report = Report(
+            userId,
+            location,
+            date,
+            time,
+            incidentDetails
+        )
+
+        fStore.collection("reports").document().set(report)
+
+        Toast.makeText(this@ReportAccidentActivity, "Report successful!", Toast.LENGTH_SHORT).show()
+
+        finish()
     }
 }
