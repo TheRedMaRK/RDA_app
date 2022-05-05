fcd54f08cb2243a59c81703722b5ce52922d25d5
TheRedMaRK <pmmrkumaradasa@students.nsbm.ac.lk>
2022-05-05 12:39:18 +0530

    - Logged in users are always logged in now. - Attempted to add the logout button inside a navigation drawer. The logout button works but the navigation drawer isn't working somehow

--- app/src/main/java/com/example/rda_app/HomeUserActivity.kt
+++ app/src/main/java/com/example/rda_app/HomeUserActivity.kt
@@ -1,17 +1,20 @@
 package com.example.rda_app
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.rda_app.databinding.ActivityHomeUserBinding
-import com.example.rda_app.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home_user.*
 
 class HomeUserActivity : AppCompatActivity() {
     // view binding
     private lateinit var binding: ActivityHomeUserBinding
     //Gives the ability open the drawer by the Toggle button
     private lateinit var toggle: ActionBarDrawerToggle
+    //FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,5 +37,25 @@ class HomeUserActivity : AppCompatActivity() {
             val intent = Intent(this, MyReportsActivity::class.java)
             startActivity(intent)
         }
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //For the drawerLayout to work we should include: id 'kotlin-android-extensions' in build.gradle(Module) plugins section
+        toggle = ActionBarDrawerToggle(this@HomeUserActivity, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.miItem1 -> {
+                    firebaseAuth.signOut()
+                    startActivity(Intent(this,LoginActivity::class.java))
+                    finish()
+                }
+            }
+            true
+        }
+
     }
 }
--- app/src/main/java/com/example/rda_app/WelcomeActivity.kt
+++ app/src/main/java/com/example/rda_app/WelcomeActivity.kt
@@ -3,22 +3,92 @@ package com.example.rda_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
 import kotlin.concurrent.schedule
 
 class WelcomeActivity : AppCompatActivity() {
+
+    // Firestore variable
+    private lateinit var fStore: FirebaseFirestore
+
+    // FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
+        //init firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+
         Timer().schedule(1000) {
-            goToLogin()
+            checkUser()
         }
     }
 
-    fun goToLogin() {
-        val intent = Intent(this@WelcomeActivity, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
+    private fun checkUser() {
+        //if user is already logged in go to Main activity
+        //get current user
+        if (firebaseAuth.currentUser != null){
+            val userId = firebaseAuth.currentUser!!.uid
+            val email = firebaseAuth.currentUser!!.email
+            //Code to get if userLogin variable true or false from the database
+            fStore = FirebaseFirestore.getInstance()
+            fStore.collection("users").document(userId).get().addOnCompleteListener { task: Task<DocumentSnapshot> ->
+                val document = task.result
+                val username = document.get("fullName").toString()
+                val vehicleId = document.get("regNo").toString()
+
+                Toast.makeText(this,"Logged in as $email", Toast.LENGTH_SHORT).show()
+
+                when (document.get("type").toString()) {
+                    "admin" -> {
+                        val intent = Intent(this, WebmasterActivity::class.java)
+                        intent.putExtra("name", username)
+                        startActivity(intent)
+                        finish()
+                    }
+                    "driver" -> {
+                        val intent = Intent(this, HomeUserActivity::class.java)
+                        intent.putExtra("name", username)
+                        intent.putExtra("vid", vehicleId)
+                        startActivity(intent)
+                        finish()
+                    }
+                    "police" -> {
+                        val intent = Intent(this, HomeStaffActivity::class.java)
+                        intent.putExtra("user", "police")
+                        intent.putExtra("name", username)
+                        startActivity(intent)
+                        finish()
+                    }
+                    "rda" -> {
+                        val intent = Intent(this, HomeStaffActivity::class.java)
+                        intent.putExtra("user", "rda")
+                        intent.putExtra("name", username)
+                        startActivity(intent)
+                        finish()
+                    }
+                    "insurance" -> {
+                        val intent = Intent(this, HomeStaffActivity::class.java)
+                        intent.putExtra("user", "insurance")
+                        intent.putExtra("name", username)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+
+        }
+        else{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
