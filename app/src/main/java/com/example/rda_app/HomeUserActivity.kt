4eb89664e6206198a624438fb7a7048fa34d700a
TheRedMaRK <pmmrkumaradasa@students.nsbm.ac.lk>
2022-05-05 11:44:29 +0530

    - Added Edit and Delete buttons for my_reports_layout.xml. They still don't function

--- app/src/main/java/com/example/rda_app/HomeUserActivity.kt
+++ app/src/main/java/com/example/rda_app/HomeUserActivity.kt
@@ -3,12 +3,15 @@ package com.example.rda_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.rda_app.databinding.ActivityHomeUserBinding
 import com.example.rda_app.databinding.ActivityLoginBinding
 
 class HomeUserActivity : AppCompatActivity() {
     // view binding
     private lateinit var binding: ActivityHomeUserBinding
+    //Gives the ability open the drawer by the Toggle button
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
