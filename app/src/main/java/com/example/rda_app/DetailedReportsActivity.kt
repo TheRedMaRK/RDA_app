512cfbce7c0bf771b2475579da1f72618dca0114
TheRedMaRK <pmmrkumaradasa@students.nsbm.ac.lk>
2022-05-11 10:52:40 +0530

    - Reverted the charts. Found it too complex and unnecessary

--- app/src/main/java/com/example/rda_app/DetailedReportsActivity.kt
+++ app/src/main/java/com/example/rda_app/DetailedReportsActivity.kt
@@ -4,11 +4,6 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rda_app.databinding.ActivityDetailedReportsBinding
-import com.github.mikephil.charting.animation.Easing
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import kotlinx.android.synthetic.main.activity_detailed_reports.*
 
 class DetailedReportsActivity : AppCompatActivity() {
 
@@ -50,49 +45,5 @@ class DetailedReportsActivity : AppCompatActivity() {
             binding.textView2.text =
                 "Filter: " + spinnerFilter.selectedItem.toString() + "\nChart type: " + spinnerChart.selectedItem.toString()
         }
-
-        // Charts
-
-        //Part1
-        val entries = ArrayList<Entry>()
-
-        //Part2
-        entries.add(Entry(1f, 10f))
-        entries.add(Entry(2f, 2f))
-        entries.add(Entry(3f, 7f))
-        entries.add(Entry(4f, 20f))
-        entries.add(Entry(5f, 16f))
-
-        //Part3
-        val vl = LineDataSet(entries, "My Type")
-
-        //Part4
-        vl.setDrawValues(false)
-        vl.setDrawFilled(true)
-        vl.lineWidth = 3f
-        vl.fillColor = R.color.primaryColor
-        vl.fillAlpha = R.color.secondaryColor
-
-        //Part5
-        lineChart.xAxis.labelRotationAngle = 0f
-
-        //Part6
-        lineChart.data = LineData(vl)
-
-        //Part7
-        lineChart.axisRight.isEnabled = false
-        val j = 1
-        lineChart.xAxis.axisMaximum = j + 0.1f
-
-        //Part8
-        lineChart.setTouchEnabled(true)
-        lineChart.setPinchZoom(true)
-
-        //Part9
-        lineChart.description.text = "Days"
-        lineChart.setNoDataText("No forex yet!")
-
-        //Part10
-        lineChart.animateX(1800, Easing.EaseInExpo)
     }
 }
